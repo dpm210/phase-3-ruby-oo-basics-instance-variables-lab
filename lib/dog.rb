27fa771c0bfa518c033296787ff62639e17bdf21
Daniel Mueller <danielpetermueller@gmail.com>
2021-06-29 12:31:52 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
 
+    attr_accessor :name
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    def name
+        @this_dogs_name
+    end
+
+    lassie = Dog.new
 end
